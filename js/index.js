595666ae4c76057f0166a4d9e44b562ebbc72e3e
Bhawan Sidhu <55447886+bhawansidhu@users.noreply.github.com>
2019-12-04 23:31:24 -0500

    add code

---
+++ js/index.js
@@ -0,0 +1,636 @@
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    discount: 20,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt2 = {
+    name: `Red Shirt`,
+    imageUrl: `img/red shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    discount: 20,
+    productCode: 2502,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt3 = {
+    name: `white Shirt`,
+    imageUrl: `img/white shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+const shirt1 = {
+    name: `Blue Shirt`,
+    imageUrl: `img/blue shirt.jpg`,
+    colors: [`white`, `blue`, `balck` , `red`],
+    productOnSale: true,
+    productCode: 2501,
+    brand: `Zara`,
+    category: `Formal`,
+    price: `90`,
+    rating: 4.3,
+    
+};
+  
+  
+  
+  const nike = [
+    nike1,
+    nike2,
+    nike3,
+    nike4,
+    nike5,
+    nike6,
+    nike7,
+    nike8,
+    nike9,
+    nike10,
+    nike11
+  ];
+  
+  const puma1 = {
+    name: `Kor Sneakers `,
+    imageUrl: `img/puma/casual/puma-casual%20(1).jpeg`,
+    productCode: 111,
+    category: `casual`,
+    price: `210`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9,10],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma2 = {
+    name: `Poise Perf IDP`,
+    imageUrl: `img/puma/casual/puma-casual%20(2).jpeg`,
+    productCode: 112,
+    category: `casual`,
+    price: `110`,
+    rating: 4.2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 6,
+    sizes:[8, 9],
+    discount: 5,
+    onSale: false
+  };
+  
+  const puma3 = {
+    name: `Icon IDP Canvas `,
+    imageUrl: `img/puma/casual/puma-casual%20(3).jpeg`,
+    productCode: 113,
+    category: `casual`,
+    price: `180`,
+    rating: 3.7,
+    brand: `puma`,
+    colors: [`red`, `blue`,'black'],
+    stock: 112,
+    sizes: [6,7, 8],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma4 = {
+    name: `puma shoes 4`,
+    imageUrl: `img/puma/casual/puma-casual%20(4).jpeg`,
+    productCode: `PUMA103`,
+    category: `casual`,
+    price: `130`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma5 = {
+    name: `puma shoes 5`,
+    imageUrl: `img/puma/running/puma-running%20(1).jpeg`,
+    productCode: `PUMA104`,
+    category: `running`,
+    price: `140`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: false
+  };
+  
+  const puma6 = {
+    name: `puma shoes 6`,
+    imageUrl: `img/puma/running/puma-running%20(2).jpeg`,
+    productCode: `PUMA105`,
+    category: `running`,
+    price: `150`,
+    rating: 1,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma7 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/running/puma-running%20(3).jpeg`,
+    productCode: `PUMA106`,
+    category: `running`,
+    price: `160`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma8 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/running/puma-running%20(4).jpeg`,
+    productCode: `PUMA107`,
+    category: `running`,
+    price: `170`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: false
+  };
+  
+  const puma9 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/running/puma-running%20(4).jpeg`,
+    productCode: `PUMA108`,
+    category: `running`,
+    price: `180`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: false
+  };
+  
+  const puma10 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/running/puma-running%20(5).jpeg`,
+    productCode: `PUMA109`,
+    category: `running`,
+    price: `190`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: false
+  };
+  
+  const puma11 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/sports/puma-sports%20(1).jpeg`,
+    productCode: `PUMA110`,
+    category: `sports`,
+    price: `120`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma12 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/sports/puma-sports%20(2).jpeg`,
+    productCode: `PUMA111`,
+    category: `sports`,
+    price: `200`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: true
+  };
+  
+  const puma13 = {
+    name: `puma shoes 7`,
+    imageUrl: `img/puma/sports/puma-sports%20(3).jpeg`,
+    productCode: `PUMA110`,
+    category: `sports`,
+    price: `210`,
+    rating: 2,
+    brand: `puma`,
+    colors: [`red`, `blue`],
+    stock: 10,
+    sizes: [7, 8, 9],
+    discount: 5,
+    onSale: true
+  };
+  const puma = [
+    puma1,
+    puma2,
+    puma3,
+    puma4,
+    puma5,
+    puma6,
+    puma7,
+    puma8,
+    puma9,
+    puma10,
+    puma11,
+    puma12,
+    puma13
+  ];
+  
+  const shoesData = nike.concat(puma);
+  
+  //functions
+  function getShoesAsHtml(shoesData) {
+    return `
+      <section class="productsCard">
+      <section class="container">
+      <img clas="productCardImg" src="${shoesData.imageUrl}" alt="${shoesData.name}" style="width:100%;height:130px;">
+      <button type="button" class="heartBtn" id="heartBtn"><span class="material-icons">favorite</span></button>
+      </section>
+      <h1 class="ProductCardName"><strong>${shoesData.name}</strong></h1>
+          <ul class="productCardList">
+            <li class="productCardListItem"><h3>${shoesData.category}</h3></li>
+            <li class="productCardListItem"><h3>Price: $${shoesData.price}</h3></li>
+            <li class="productCardListItem"><p>Average rating: ${shoesData.rating}<p></li>
+          </ul>
+          <button type="button" class="productCardButton" id="addtocart"><span class="material-icons">add_shopping_cart</span> Add to Cart</button> 
+          </section>`;
+  }
+  
+  document.getElementById(`app`).innerHTML = shoesData
+    .map(getShoesAsHtml)
+    .join("\n");
+  
+    //search functionality
+  function LoadProductsByName(){
+    const search=document.getElementById('find').value;
+    const lowerCase=search.trim().toLowerCase();
+    const searchResultsArray=shoesData.filter(p=> p.name.toLowerCase().includes(lowerCase));
+    renderProducts(searchResultsArray);
+  }
+  
+  function LoadFilterMenu(){
+    const x = document.getElementById("filtermenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  //sorting functions
+  function LoadProductsPriceHighTolow() {
+    alert("in high to low");
+    const PriceHighToLowArray = shoesData
+      .slice(0)
+      .sort((a, b) => (a.price < b.price ? 1 : -1));
+    renderProducts(PriceHighToLowArray);
+  }
+  
+  function LoadProductsPriceLowToHigh() {
+    alert("in low to high");
+    const PriceLowToHighArray = shoesData
+      .slice(0)
+      .sort((a, b) => (a.price > b.price ? 1 : -1));
+    renderProducts(PriceLowToHighArray);
+  }
+  
+  function LoadProductsByAvailability() {
+    alert("in availability function");
+    const availabilityArray = shoesData
+      .slice(0)
+      .sort((a, b) => (a.stock < b.stock ? 1 : -1));
+    renderProducts(availabilityArray);
+  }
+  
+  function LoadProductsByRatings() {
+    alert("in rating function");
+    const ratingArray = shoesData
+      .slice(0)
+      .sort((a, b) => (a.rating < b.rating ? 1 : -1));
+    renderProducts(ratingArray);
+  }
+  
+  function LoadProductsOnSale(){
+    alert("in onsale function");
+    const onSaleArray= shoesData.slice(0).filter(p => p.onSale == true);
+    renderProducts(onSaleArray);
+  }
+  
+  //filter functions
+  
+  
+  
+  const addToClickToHeartBtn = element => {
+    const addToWishlist = () => {
+      if (element.style.color != 'red') {
+        element.style.color = 'red';
+      } else {
+        element.style.color = 'rgb(63, 46, 46)'
+      };
+    }
+  
+    element.addEventListener('click', addToWishlist)
+  }
+  
+  const addToClickToCartBtn = element => {
+    const addTOCart = () => {
+      if (element.style.backgroundColor != `red` && element.innerHTML != `Remove from Cart`) {
+        element.style.backgroundColor = `red`;
+        element.innerHTML = `Remove from Cart`;
+      } else {
+        element.style.backgroundColor = `rgba(0, 0, 0, .87)`;
+        element.innerHTML = `Add to Cart`;
+      }
+  
+    }
+    element.addEventListener('click', addTOCart)
+  }
+  
+  
+  function renderProducts(arr) {
+    document.getElementById(`app`).innerHTML = arr.map(getShoesAsHtml).join("\n");
+    const heartBtn = document.querySelectorAll(`.heartBtn`);
+    heartBtn.forEach(addToClickToHeartBtn);
+    const AddtoCartBtn = document.querySelectorAll(`.productCardButton`);
+    AddtoCartBtn.forEach(addToClickToCartBtn);
+    let res = 'Products';
+    if (arr.length == 1) {
+      res = 'Product'
+    }
+    //let res = (arr.length > 1) ? 'results' : 'result';
+  
+    document.getElementById('numResults').innerHTML = `(${arr.length} ${res})`;
+   
+  }
+  
+  
+  
+  
+  //execution
+  window.addEventListener('load', () => {
+  
+  const heartBtn = document.querySelectorAll(`.heartBtn`);
+  heartBtn.forEach(addToClickToHeartBtn);
+  const AddtoCartBtn = document.querySelectorAll(`.productCardButton`);
+  AddtoCartBtn.forEach(addToClickToCartBtn);
+  document.getElementById(`find`).addEventListener("change",LoadProductsByName);
+  document.getElementById(`PriceHighToLow`).addEventListener("click", LoadProductsPriceHighTolow);
+  document.getElementById(`PriceLowToHigh`).addEventListener("click", LoadProductsPriceLowToHigh);
+  document.getElementById(`availability`).addEventListener("click", LoadProductsByAvailability);
+  document.getElementById(`topRated`).addEventListener("click", LoadProductsByRatings);
+  document.getElementById(`onSale`).addEventListener("click", LoadProductsOnSale);
+  document.getElementById(`filter`).addEventListener("click", LoadFilterMenu);
+  
+  //start
+  getShoesAsHtml(shoesData);
+  });  
